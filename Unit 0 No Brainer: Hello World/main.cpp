68f3270264cac23441ef360fb4a841e124c262ea
Coleton Watt <electricninja102@gmail.com>
2021-08-30 09:52:14 -0600

    Hello World

--- Unit 0 No Brainer: Hello World/main.cpp
+++ Unit 0 No Brainer: Hello World/main.cpp
@@ -4,11 +4,10 @@
 //
 //  Created by Coleton Watt on 8/30/21.
 //
-
 #include <iostream>
+using std::cout;
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
-    return 0;
+int main() {
+     cout << "Hello World\n";
+     return 0;
 }
